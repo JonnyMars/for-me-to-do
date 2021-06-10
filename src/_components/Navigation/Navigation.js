db101348b2f79dde4d05b89182d81cdfb242bce1
Jonny Mars <jonny.mars@iprospect.com>
2021-06-10 22:55:46 +0100

    upd: remove sepearate login/signup button and combined

--- src/_components/Navigation/Navigation.js
+++ src/_components/Navigation/Navigation.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from "./Navigation.module.scss";
 
 
 export default function Navigation(props) {
+    
+    let loginBtn = <Link to="login" className={styles.Button}>Login / Sign Up</Link>
 
-    const signupBtn = <Link to="signup" className={[styles.Button, styles.SignUpButton].join(" ")}>Sign Up</Link>
-    const loginBtn = <Link to="login" className={styles.Button}>Login</Link>
+    const location = useLocation();
+    if(location.pathname === ("/login" || "/signup")) loginBtn = null;
+
 
     return (
         <nav className={`${styles.Navigation} full-width`}>
@@ -18,7 +21,6 @@ export default function Navigation(props) {
                     <span>Do</span>
                 </Link>
                 <div>
-                    {signupBtn}
                     {loginBtn}
                 </div>
             </div>
